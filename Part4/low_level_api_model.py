5f09395d68bdefd8aa3eccb33935498ebc0e47ed
ramoomar99 <rtaha4@gmail.com>
2020-02-16 02:08:40 -0600

    create file for low level api model

---
+++ Part4/low_level_api_model.py
@@ -0,0 +1,11 @@
+import warnings
+warnings.filterwarnings('ignore',category=FutureWarning)
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+	pass
